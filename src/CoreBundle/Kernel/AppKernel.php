03fc9454a6b34c85ac013f4ccdf6a80371ec8cf2
jivoy1988 <jivoy1988@gmail.com>
2017-01-19 15:23:32 +0500

    Up
    Former-commit-id: cf31a190f65eae0079141cfbe758b3595aea2b55

--- src/CoreBundle/Kernel/AppKernel.php
+++ src/CoreBundle/Kernel/AppKernel.php
@@ -104,7 +104,6 @@ class AppKernel extends Kernel
             new \Compo\OrderBundle\CompoOrderBundle(),
             new \Compo\BasketBundle\CompoBasketBundle(),
             new \Compo\TaggingBundle\CompoTaggingBundle(),
-            new \Compo\DiscountBundle\CompoDiscountBundle(),
 
             new \JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new \JMS\AopBundle\JMSAopBundle(),
