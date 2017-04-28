400438d0da1855263d5c0a2c1d2df2df9a98bc3a
jivoy1988 <jivoy1988@gmail.com>
2017-04-28 18:49:11 +0500

    Fix filter.
    Former-commit-id: 73dfa8976e1dfacc81798507b946cfd19fb0be47

---
+++ src/CoreBundle/Composer/BowerInstall.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Compo\CoreBundle\Composer;
+
+use Composer\Script\Event;
+use Symfony\Component\Process\Process;
+
+class BowerInstall
+{
+    public static function process(Event $event)
+    {
+        $io = $event->getIO();
+
+        $extras = $event->getComposer()->getPackage()->getExtra();
+
+        if (!isset($extras['bower'])) {
+            throw new \InvalidArgumentException('The parameter handler needs to be configured through the extra.bower setting.');
+        }
+
+        $paths = $extras['bower'];
+
+        if (!is_array($paths)) {
+            throw new \InvalidArgumentException('The bower setting must be an array or a configuration object.');
+        }
+
+        $vendor = $event->getComposer()->getConfig()->get('vendor-dir');
+
+        $root_dir = realpath($vendor . '/../');
+
+        foreach ($paths as $path) {
+            $io->write(sprintf('<info>bower: %s</info>', $path));
+
+            $process = new Process('bower install ' . $root_dir . '/' . $path, $root_dir, null, null, 300);
+
+            $process->run(function ($type, $buffer) use ($event) { $event->getIO()->write($buffer, false); });
+
+            if (!$process->isSuccessful()) {
+                throw new \RuntimeException('An error occurred when bower.');
+            }
+        }
+    }
+}
