2ee637ecb92800c04832fd439f38f53eebfb8c1d
jivoy1988 <jivoy1988@gmail.com>
2017-05-15 16:50:08 +0500

    SonataFormatter bug: https://github.com/sonata-project/SonataFormatterBundle/issues/248
    Former-commit-id: 8cf6550de8a2fe1f443e1129623b805658d7b5c9

--- src/Compo/Sonata/FormatterBundle/Block/FormatterBlockService.php
+++ src/Compo/Sonata/FormatterBundle/Block/FormatterBlockService.php
@@ -2,9 +2,17 @@
 
 namespace Compo\Sonata\FormatterBundle\Block;
 
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Sonata\BlockBundle\Block\Service\AbstractAdminBlockService;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\CoreBundle\Model\Metadata;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FormatterBlockService extends \Sonata\FormatterBundle\Block\FormatterBlockService
+class FormatterBlockService extends AbstractAdminBlockService
 {
     /**
      * {@inheritdoc}
@@ -16,6 +24,49 @@ class FormatterBlockService extends \Sonata\FormatterBundle\Block\FormatterBlock
             'rawContent' => '',
             'content' => '',
             'template' => 'SonataFormatterBundle:Block:block_formatter.html.twig',
+        ));
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        return $this->renderResponse($blockContext->getTemplate(), array(
+            'block' => $blockContext->getBlock(),
+            'settings' => $blockContext->getSettings(),
+        ), $response);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formMapper->add('settings', 'sonata_type_immutable_array', array(
+            'keys' => array(
+                array('content', 'sonata_formatter_type', function (FormBuilderInterface $formBuilder) {
+                    return array(
+                        'event_dispatcher' => $formBuilder->getEventDispatcher(),
+                        'format_field' => array('format', '[format]'),
+                        'source_field' => array('rawContent', '[rawContent]'),
+                        'target_field' => '[content]',
+                        'label' => 'form.label_content',
+                    );
+                }),
+            ),
+            'translation_domain' => 'SonataFormatterBundle',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockMetadata($code = null)
+    {
+        return new Metadata($this->getName(), (!is_null($code) ? $code : $this->getName()), false, 'SonataFormatterBundle', array(
+            'class' => 'fa fa-file-text-o',
         ));
     }
 }
