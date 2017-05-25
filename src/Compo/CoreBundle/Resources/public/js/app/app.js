9c405bd806c2ce50594461c77d4bbc40e14d4814
gun2rin <gun2rin@gmail.com>
2017-05-25 23:11:24 +0300

    Изменения в архитектуре Angular приложений - update
    Former-commit-id: aa8f9ecb76d6d2fca1604a265f3fc049b92f6a2c

--- src/Compo/CoreBundle/Resources/public/js/app/app.js
+++ src/Compo/CoreBundle/Resources/public/js/app/app.js
@@ -2,13 +2,6 @@
     'use strict';
     /* @ngInject */
     angular
-        .module('app', ['app.ecommerce','app.contacts'])
-        .config(['$httpProvider', function($httpProvider) {
-            $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-        }])
-        .filter('unsafe', ['$sce', function($sce){
-            return function(text) {
-                return $sce.trustAsHtml(text);
-            };
-        }]);
+        .module('app.core', ['app.ecommerce','app.contacts']);
+
 })();
