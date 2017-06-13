93ff677fa4bd2b913e48c12d1c5354d743ca9016
jivoy1988 <jivoy1988@gmail.com>
2017-06-13 17:49:28 +0500

    Small fixes.
    Former-commit-id: 67538cc480d4d3a3b75635c0e597c6bc44d84890

---
+++ src/Compo/FeedbackBundle/Form/CallbackFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Compo\FeedbackBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * {@inheritDoc}
+ */
+class CallbackFormType extends FeedbackBaseFormType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('phone', TextType::class)
+            ->add('message', TextareaType::class);
+    }
+}
