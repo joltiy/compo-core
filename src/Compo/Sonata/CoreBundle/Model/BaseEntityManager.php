3e8750f461748fd214c5ed8fe768d2201ca1cb23
jivoy1988 <jivoy1988@gmail.com>
2017-07-19 15:11:10 +0500

    Up
    Former-commit-id: 9879f24aa1aa11a81e93eb035f78f9f47433817a

--- src/Compo/Sonata/CoreBundle/Model/BaseEntityManager.php
+++ src/Compo/Sonata/CoreBundle/Model/BaseEntityManager.php
@@ -4,6 +4,7 @@ namespace Compo\Sonata\CoreBundle\Model;
 
 
 use Compo\CoreBundle\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\Request;
 
 class BaseEntityManager extends \Sonata\CoreBundle\Model\BaseEntityManager
 {
@@ -14,7 +15,20 @@ class BaseEntityManager extends \Sonata\CoreBundle\Model\BaseEntityManager
      */
     public function getRequest()
     {
-        return $this->getContainer()->get('request_stack')->getCurrentRequest();
+
+        $request_stack = $this->getContainer()->get('request_stack');
+
+        if ($request_stack) {
+            $request =  $request_stack->getCurrentRequest();
+
+            if ($request) {
+                return $request;
+            } else {
+                return new Request();
+            }
+        } else {
+            return new Request();
+        }
     }
 
     public function getChoices() {
