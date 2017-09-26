415b0d0927e9d39f4f79bdeb897d4619de03e538
jivoy1988 <jivoy1988@gmail.com>
2017-09-26 14:01:50 +0500

    Валидация форм.

--- src/Compo/CoreBundle/Resources/public/js/app/app.js
+++ src/Compo/CoreBundle/Resources/public/js/app/app.js
@@ -2,6 +2,39 @@
     'use strict';
     /* @ngInject */
     angular
-        .module('app.core', ['app.feedback']);
+        .module('app.core', ['app.feedback', 'validation', 'validation.rule'])
+        .config(['$validationProvider', function (validationProvider) {
+            validationProvider.setExpression({
+
+            });
+            validationProvider.setDefaultMsg({
+                required: {
+                    error: 'Обязательное!',
+                    success: 'It\'s Required'
+                },
+                url: {
+                    error: 'Неверный формат URL',
+                    success: 'It\'s Url'
+                },
+                email: {
+                    error: 'Неверный формат E-mail',
+                    success: 'It\'s Email'
+                },
+                number: {
+                    error: 'Должно быть числом',
+                    success: 'It\'s Number'
+                },
+                minlength: {
+                    error: 'Слишком короткое',
+                    success: 'Long enough!'
+                },
+                maxlength: {
+                    error: 'Слишком длиное',
+                    success: 'Short enough!'
+                }
+            });
+        }])
+
+    ;
 
 })();
