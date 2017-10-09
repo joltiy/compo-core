66fd1105eb9782b7112c2b9d9972fc398097780b
jivoy1988 <jivoy1988@gmail.com>
2017-10-09 11:35:45 +0500

    Fix fos_js_routes.js

--- src/Compo/CoreBundle/Composer/SyliusInstall.php
+++ src/Compo/CoreBundle/Composer/SyliusInstall.php
@@ -14,6 +14,12 @@ class SyliusInstall extends \Sensio\Bundle\DistributionBundle\Composer\ScriptHan
      */
     public static function process(Event $event)
     {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $vendor = $event->getComposer()->getConfig()->get('vendor-dir');
+
+        /** @noinspection RealpathInSteamContextInspection */
+        $root_dir = realpath($vendor . '/../app/../');
+
         $options = self::getOptions($event);
         $consoleDir = self::getConsoleDir($event, 'SyliusInstall');
 
@@ -21,7 +27,19 @@ class SyliusInstall extends \Sensio\Bundle\DistributionBundle\Composer\ScriptHan
 
         static::executeCommand($event, $consoleDir, 'sylius:theme:assets:install' . $extraParam, $options['process-timeout']);
 
-        static::executeCommand($event, $consoleDir, 'fos:js-routing:dump', $options['process-timeout']);
+
+        //       "Mopa\\Bundle\\BootstrapBundle\\Composer\\ScriptHandler::postInstallSymlinkTwitterBootstrap",
+        // "Mopa\\Bundle\\BootstrapBundle\\Composer\\ScriptHandler::postInstallSymlinkTwitterBootstrapSass",
+        static::executeCommand($event, $consoleDir, 'fos:js-routing:dump --target='.$root_dir . '/web/assetic/fos_js_routes.js', $options['process-timeout']);
+        static::executeCommand($event, $consoleDir, 'fos:js-routing:dump --target='.$root_dir . '/web/assetic/fos_js_routes.js', $options['process-timeout']);
+
+        static::executeCommand($event, $consoleDir, 'mopa:bootstrap:symlink:sass', $options['process-timeout']);
+
+        copy(
+            $consoleDir . '/../' . 'vendor/mopa/bootstrap-bundle/Mopa/Bundle/BootstrapBundle/Resources/public/sass/mopabootstrapbundle-3.2.scss',
+            $consoleDir . '/../' . 'vendor/mopa/bootstrap-bundle/Mopa/Bundle/BootstrapBundle/Resources/public/sass/mopabootstrapbundle.scss'
+
+        );
 
         static::executeCommand($event, $consoleDir, 'assetic:dump' . ' --forks=8 --env=dev', $options['process-timeout']);
         static::executeCommand($event, $consoleDir, 'assetic:dump' . ' --forks=8 --env=prod --no-debug', $options['process-timeout']);
