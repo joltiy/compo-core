cc91b885e4e9b960235b2a3ce62a990eee715dae
jivoy1988 <jivoy1988@gmail.com>
2017-10-09 21:51:18 +0500

    Add command for generate ide-twig.json, for themes.

---
+++ src/Compo/CoreBundle/Command/TwigIdeGenerateCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Compo\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+class TwigIdeGenerateCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('compo:core:twig:ide_generate')
+            ->setDescription('Hello PhpStorm');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $projectDir = $this->getContainer()->get('kernel')->getProjectDir();
+
+        $themesDir = $projectDir . '/app/themes';
+
+        $finder = new Finder();
+        $themesDirs = $finder->directories()->in($themesDir)->depth('== 0');
+
+        $ideTwigItems = array();
+
+        /** @var SplFileInfo $themesDirsItem */
+        foreach ($themesDirs as $themesDirsItem) {
+            $output->writeln('Theme: ' . $themesDirsItem->getBasename());
+
+            $bundles = $finder->directories()->in($themesDirsItem->getRealPath())->depth('== 0');
+
+            /** @var SplFileInfo $bundlesItem */
+            foreach ($bundles as $bundlesItem) {
+                dump($bundlesItem->getRealPath());
+                $ideTwigItems[] = array(
+                    'namespace' => $bundlesItem->getBasename(),
+                    'path' => 'app/themes/' . $themesDirsItem->getBasename() . '/' . $bundlesItem->getBasename() . '/views',
+                    'type' => 'Bundle',
+                );
+            }
+        }
+
+        $ideTwig = array(
+            'namespaces' => $ideTwigItems
+        );
+
+        $fs = new Filesystem();
+
+        $fs->dumpFile($projectDir . '/ide-twig.json', json_encode($ideTwig, JSON_PRETTY_PRINT));
+    }
+}
