89de86bfe8f0f9810b6d7c2cc547340bf0217c9c
jivoy1988 <jivoy1988@gmail.com>
2017-11-05 21:14:34 +0500

    Test

--- src/Compo/CoreBundle/Resources/recipe/compo.php
+++ src/Compo/CoreBundle/Resources/recipe/compo.php
@@ -301,7 +301,10 @@ task(
         run('cd {{release_path}} && ' . trim(get('bin_dir'), '/') . '/behat --format html --format=pretty');
 
         // TODO: Fix...
-        run('cd {{release_path}} && sed -i -- \'s/<script type=\'text/javascript\' src=\'https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js\'></script>//g\' build/behat/index.html');
+        run('cd {{release_path}} && sed -i -- \'s/<script type=\'text\/javascript\' src=\'https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.3\/jquery.min.js\'></script>//g\' build/behat/index.html');
+        run('cd {{release_path}} && sed -i -- \'s/http:\/\//https:\/\//g\' build/behat/index.html');
+
+
     }
 );
 
