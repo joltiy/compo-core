f014fc54b1d2c8795b4e5303c926c656a4a1d89c
jivoy1988 <jivoy1988@gmail.com>
2017-11-06 08:29:36 +0500

    Add JobQueueBundle for Cron commands

--- src/Compo/SeoBundle/Command/SitemapsDumpCommand.php
+++ src/Compo/SeoBundle/Command/SitemapsDumpCommand.php
@@ -2,6 +2,8 @@
 
 namespace Compo\SeoBundle\Command;
 
+use JMS\JobQueueBundle\Console\CronCommand;
+use JMS\JobQueueBundle\Entity\Job;
 use Presta\SitemapBundle\Service\DumperInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -14,8 +16,18 @@ use Symfony\Component\Console\Output\OutputInterface;
  *
  * @author Konstantin Tjuterev <kostik.lv@gmail.com>
  */
-class SitemapsDumpCommand extends ContainerAwareCommand
+class SitemapsDumpCommand extends ContainerAwareCommand implements CronCommand
 {
+    public function shouldBeScheduled(\DateTime $lastRunAt)
+    {
+        return time() - $lastRunAt->getTimestamp() >= 60 * 60 * 12;
+    }
+
+    public function createCronJob(\DateTime $lastRunAt)
+    {
+        return new Job('compo:sitemaps:dump');
+    }
+
     /**
      * Configure CLI command, message, options
      *
