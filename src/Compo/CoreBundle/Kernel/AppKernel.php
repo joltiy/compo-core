f0c152a1420ce376d66ee2548a80d111e3abfda9
jivoy1988 <jivoy1988@gmail.com>
2017-11-10 05:58:35 +0500

    Small fix

--- src/Compo/CoreBundle/Kernel/AppKernel.php
+++ src/Compo/CoreBundle/Kernel/AppKernel.php
@@ -146,7 +146,6 @@ class AppKernel extends Kernel
             $bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new \Symfony\Bundle\WebServerBundle\WebServerBundle();
-
         }
 
         return $bundles;
