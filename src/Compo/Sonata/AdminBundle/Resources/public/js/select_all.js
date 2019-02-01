796c0679d17fb1fb4e37f9b0f34b22ac866af49e
gun2rin <gun2rin@gmail.com>
2019-02-01 14:14:53 +0300

    [fix] window.SONATA_CONFIG not defined

--- src/Compo/Sonata/AdminBundle/Resources/public/js/select_all.js
+++ src/Compo/Sonata/AdminBundle/Resources/public/js/select_all.js
@@ -5,7 +5,7 @@ $(function ($) {
         $('.select-all-btn', data.subject).on('ifChanged change', function () {
             var checkboxes = $(this).closest('.select-all-wrap').find(':checkbox').not($(this));
 
-            if (window.SONATA_CONFIG.USE_ICHECK) {
+            if (window.SONATA_CONFIG && window.SONATA_CONFIG.USE_ICHECK) {
                 checkboxes.iCheck($(this).is(':checked') ? 'check' : 'uncheck');
             } else {
                 checkboxes.prop('checked', $(this).is(':checked'));
