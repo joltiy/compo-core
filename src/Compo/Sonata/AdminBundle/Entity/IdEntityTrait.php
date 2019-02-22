f06ae31b16df29c02cc4c86369c90e26383d88fe
liondrow <seredyan007@mail.ru>
2019-02-22 17:39:52 +0300

    MIR-1 #comment Задачи тестовой версии

--- src/Compo/Sonata/AdminBundle/Entity/IdEntityTrait.php
+++ src/Compo/Sonata/AdminBundle/Entity/IdEntityTrait.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
 
 /**
- * ID.
+ * Trait IdEntityTrait.
  */
 trait IdEntityTrait
 {
--- src/Compo/Sonata/AdminBundle/Extension/ColorExtension.php
+++ src/Compo/Sonata/AdminBundle/Extension/ColorExtension.php
@@ -1,25 +1,20 @@
 <?php
 
-/*
- * This file is part of the CompoSymfonyCms package.
- * (c) Compo.ru <info@compo.ru>
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Compo\Sonata\AdminBundle\Extension;
 
+use Compo\CoreBundle\DependencyInjection\ContainerAwareTrait;
 use Compo\Sonata\AdminBundle\Admin\AbstractAdminExtension;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
 
 /**
  * {@inheritdoc}
  */
 class ColorExtension extends AbstractAdminExtension
 {
+    use ContainerAwareTrait;
+
     /**
-     * @param DatagridMapper $datagridMapper
+     * {@inheritdoc}
      */
     public function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
@@ -31,26 +26,6 @@ class ColorExtension extends AbstractAdminExtension
 
         if (!$datagridMapper->has('color')) {
             $datagridMapper->add('color');
-        }
-    }
-
-    /**
-     * @param ListMapper $listMapper
-     */
-    public function configureListFields(ListMapper $listMapper)
-    {
-        if (!$this->isUseEntityTraits($listMapper->getAdmin(), [
-            'Compo\Sonata\AdminBundle\Entity\ColorEntityTrait',
-        ])) {
-            return;
-        }
-
-        if ($listMapper->has('color')) {
-            $keys = $listMapper->keys();
-
-            $listMapper->remove('color');
-            $listMapper->add('color', 'html');
-            $listMapper->reorder($keys);
         }
     }
 }
